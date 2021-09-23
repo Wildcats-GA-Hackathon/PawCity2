6d7caa0fd12ef89c02314eca936ba4c2f0a9784f
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-09-22 22:03:25 -0700

    Added user model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('../db/connection');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			virtuals: true,
+			transform: (_doc, ret) => {
+				delete ret.password;
+				mongoose.Schema;
+				return ret;
+			},
+		},
+	}
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
