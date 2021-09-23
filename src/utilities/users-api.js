7d2cd18f1d181db11a77dd4d4aff37612773d96b
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-09-22 23:09:04 -0700

    Include functionality to signup and login

---
+++ src/utilities/users-api.js
@@ -0,0 +1,11 @@
+import sendRequest from './send-request';
+
+const BASE_URL = '/api/users/';
+
+export function signup(userData) {
+	return sendRequest(BASE_URL, 'POST', userData);
+}
+
+export function login(userInfo) {
+	return sendRequest(BASE_URL + 'login', 'POST', userInfo);
+}
