9e3280637df918eab2dd230bb1d9b2efbf7ae2b2
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-09-22 23:05:18 -0700

    Added files in utilities and functionality for sending a request

---
+++ src/utilities/send-request.js
@@ -0,0 +1,22 @@
+export default async function sendRequest(
+	url,
+	method = 'GET',
+	payload = null,
+	payloadIsFormData = false
+) {
+	const options = { method };
+	if (payload) {
+		options.headers = { 'Content-Type': 'application/json' };
+		options.body = payloadIsFormData ? payload : JSON.stringify(payload);
+	}
+
+	const token = getToken();
+
+	if (token) {
+		options.headers = options.headers || {};
+		options.headers.Authorization = `Bearer ${token}`;
+	}
+	const res = await fetch(url, options);
+	if (res.ok) return res.json();
+	throw new Error('Bad Request');
+}
