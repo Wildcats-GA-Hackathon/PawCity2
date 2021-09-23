ed72e7f2e08ae20ad20d569d1207b4cd295c2ec9
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-09-22 21:36:03 -0700

    Installed dependencies and created a server.js file to run backend

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// authentication code
+app.use(require('./config/checkToken'));
+
+// put all the API routes HERE before the catch all route
+
+// 'catch all'
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+	console.log(`Backend up and running on port ${port}`);
+});
